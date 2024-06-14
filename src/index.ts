dbd3448feb414de43b1f88bc73b1b937cff56c93
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-06-14 10:36:42 +0200

    fix(fetch): adapt new API interface

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,43 @@ import { table } from 'table';
 import axios from 'axios';
 import fs from 'fs';
 
+type Body = { results: ProjectDomain[]; next: number | null; }
+
+const result: ProjectDomain[] = [];
+
 const directory: string = 'metrics';
 
-axios.get<ProjectDomain[]>('https://eip-tek3.epitest.eu/api/projects/', {
-  headers: {
-    'Authorization': ['Bearer', process.env.ACCESS_TOKEN].join(' ')
-  }
-})
-.then(res => {
-  const body: ProjectDomain[] = res.data;
+async function fetchProjects(offset: number): Promise<void> {
+  console.log(`Fetching projects… (offset=${offset})`);
+
+  return axios.get<Body>('https://eip-tek3.epitest.eu/api/projects/', {
+    headers: {
+      'Authorization': ['Bearer', process.env.ACCESS_TOKEN].join(' ')
+    },
+    params: {
+      offset,
+      scholar_year: 2023,
+      include_rejected: false,
+      limit: 20
+    }
+  })
+  .then(async res => {
+    const body: Body = res.data;
+
+    body.results.forEach(e => {
+      if (result.find(r => r.id === e.id))
+        return;
+      result.push(e);
+    });
+
+    if (body.next)
+      await fetchProjects(body.next);
+  })
+  .catch(error => console.error(error));
+}
+
+async function generateMetrics() {
+  await fetchProjects(0);
 
   try {
     if (!fs.existsSync(directory))
@@ -24,25 +52,28 @@ axios.get<ProjectDomain[]>('https://eip-tek3.epitest.eu/api/projects/', {
   const filename = `${Date.now()}.json`;
   const filepath: string = join(__dirname, '..', directory, filename);
 
-  fs.writeFileSync(filepath, JSON.stringify(body), 'utf-8');
+  fs.writeFileSync(filepath, JSON.stringify(result), 'utf-8');
 
-  const metrics: Project[] = body.map(e => {
+  const metrics: Project[] = result.map(e => {
     const ratio: number = e.starsCount > 0 ? (e.starsCount / e.viewsCount * 100) : 0
 
     return {
+      id: e.id,
       name: e.name,
       views: e.viewsCount,
       stars: e.starsCount,
       ratio: Math.round(ratio * 100) / 100,
-      type: e.envisagedType
+      type: e.envisagedType,
+      city: e.ownerCity.name
     }
   });
 
   const sorted: Project[] = metrics.sort((a, b) => b.ratio - a.ratio);
 
   console.log(table([
-    ['Position', 'Name', 'Views', 'Stars', 'Ratio', 'Type'],
-    ...sorted.map((e, i) => [i + 1, e.name, e.views, e.stars, e.ratio, e.type])
+    ['Position', 'Name', 'Views', 'Stars', 'Ratio', 'Type', 'Ville'],
+    ...sorted.map((e, i) => [i + 1, e.name, e.views, e.stars, e.ratio, e.type, e.city])
   ]));
-})
-.catch(error => console.error(error));
+}
+
+generateMetrics();
--- src/types/project.ts
+++ src/types/project.ts
@@ -5,16 +5,22 @@ enum ProjectType {
 }
 
 export interface ProjectDomain {
+  id: number;
   name: string;
   starsCount: number;
   viewsCount: number;
   envisagedType: ProjectType;
+  ownerCity: {
+    name: string;
+  };
 }
 
 export interface Project {
+  id: number;
   name: string;
   stars: number;
   views: number;
   ratio: number;
+  city: string;
   type: ProjectType;
 }
